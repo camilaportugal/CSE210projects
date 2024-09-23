afc25dca83aedc4c84e4223dded520a744ce5132
camilaportugal <portugalcami03@gmail.com>
2024-09-23 02:09:11 -0500

    Finish Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        string keepPlaying = "yes";
+
+        while (keepPlaying == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = 0; 
+            int guesses = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guesses += 1;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else 
+                {
+                    Console.WriteLine("Congratulations, you did it!");
+                }
+            }
+            Console.WriteLine($"It took you {guesses} guesses");
+
+            Console.Write("Would you like to play again? ");
+            keepPlaying = Console.ReadLine();
+        }
     }
 }
