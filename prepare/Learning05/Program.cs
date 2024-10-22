0562c71570ac8bc19992d3af6591411b689e6d26
camilaportugal <portugalcami03@gmail.com>
2024-10-21 19:10:09 -0500

    Finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment a1 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(a1.GetSumamry());
+
+        Console.WriteLine();
+
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSumamry());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment a3 = new WritingAssignment("Mary Water", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSumamry());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
