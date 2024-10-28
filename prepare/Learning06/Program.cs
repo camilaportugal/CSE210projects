bbd14a90285d0b8beb44187db2663905c3164d01
camilaportugal <portugalcami03@gmail.com>
2024-10-27 23:51:50 -0500

    Finished Learning06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square(3, "Red");
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle(4, 5, "White");
+        shapes.Add(s2);
+
+        Circle s3 = new Circle(6, "Green");
+        shapes.Add(s3);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}."); 
+            Console.WriteLine();
+        }
     }
 }
